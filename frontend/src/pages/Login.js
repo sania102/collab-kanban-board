90e0b16d550f3642610868fd81ce9e129f335478
sania102 <saniashamsi16@gmail.com>
2025-07-14 21:40:43 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,7 +10,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://collab-kanban-board.onrender.com/", {
         email,
         password,
       });
